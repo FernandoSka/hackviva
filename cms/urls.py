3230b83cb4de05f56de551aa57248a2baee85058
maumg1196 <maumg1196@gmail.com>
2017-09-06 00:27:41 -0500

    Add home.html

---
+++ cms/urls.py
@@ -0,0 +1,11 @@
+"""CMS' Urls."""
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+    url(regex=r'^$',
+        view=views.HomeView.as_view(),
+        name='home'),
+
+]
